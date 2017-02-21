9ab55eccec6d36237fe039327855f5058a6b0c24
Tati <eytati050@hotmail.com>
2017-02-21 09:56:02 -0600

    Aprender operaciones Aritmeticas y numeros primos
    2 clases:
    Estado actual: Route.
    Aprender: los metodos para primo y para operaciones aritmeticas.

--- Tatiana-Bot/MainDeConocimiento.py
+++ Tatiana-Bot/MainDeConocimiento.py
@@ -10,3 +10,46 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port='5001')
+
+
+
+
+class Estado_Actual:
+    def __init__(self):
+        # Constructor
+        return
+
+    @app.route('/EstadoAtual', methods=['GET'])
+    def estado(self):
+        return 'Index'
+
+class Lista_de_Conocimiento:
+
+    def modulo_aritmetico(self, operacion, numero1, numero2):
+        resultado =0
+        if operacion is 'Suma':
+           resultado= numero1 + numero2
+        elif operacion is 'Resta':
+            resultado= numero1 - numero2
+        elif operacion is 'Division':
+            resultado = numero1/numero2
+        else:
+            resultado = numero1*numero2
+        return resultado
+
+    def es_primo(self, numero):
+        primo = False
+        if numero is 2:
+            primo = True
+        elif numero%2 is 0:
+            primo = False
+        else:
+            mediaRelativa = (numero-1)/2
+            for contador in range(mediaRelativa):
+                if numero%contador is 0:
+                    primo = False
+                    break
+
+        return primo
+
+
