dda2af5e496c1c5e6916be98f6b2df67518f5ede
Tati <eytati050@hotmail.com>
2017-02-23 13:24:12 -0600

    Cambios en html y se agrego css

--- Tatiana-Bot/Camara.py
+++ Tatiana-Bot/Camara.py
@@ -2,7 +2,23 @@ import numpy as np
 import  cv2
 
 class Tomar_foto:
+    #
 
-    def Disparo_de_foto(self):
-         cv2.imshow()
-     
+
+
+
+    #def Disparo_de_foto(self):
+
+
+
+    def Mostrar_una_foto(self):
+
+
+        IMG = cv2.imread('patricio.png', 0)
+        cv2.Imshow('Patricio', IMG)
+        cv2.WhiteKey(0)
+        cv2.DestroAllWindows()
+        print(IMG)
+
+
+print(cv2)
