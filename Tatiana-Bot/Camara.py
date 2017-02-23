5ac2570e8d84d6ee676751dd19f95034cd9d1ea9
Tati <eytati050@hotmail.com>
2017-02-23 06:44:54 -0600

    Intento de foto y  creacion de memoria

---
+++ Tatiana-Bot/Camara.py
@@ -0,0 +1,8 @@
+import numpy as np
+import  cv2
+
+class Tomar_foto:
+
+    def Disparo_de_foto(self):
+         cv2.imshow()
+     
---
+++ Tatiana-Bot/Memoria.py
@@ -0,0 +1,33 @@
+class Memoria():
+
+    matriz = []
+    ultima_linea=0
+    ultima_columna = 0
+
+    def __init__(self, id_conocimiento):
+        self.matriz = self.matriz
+        return
+
+    def Agrega_linea(self):
+        self.matriz.append([])
+
+    def Agrega_columna(self, valor_de_linea):
+        self.matriz[valor_de_linea].append()
+
+    def Agregar(self):
+        for lineas in range(self.matriz):
+            self.Agrega_linea()
+            for columna in range(self.matriz):
+                self.Agrega_columna(lineas)
+
+    def Eliminar_conocimiento(self, linea, columna):
+        return
+
+    def Eliminar_conocimiento(self, id_conocimiento):
+        return
+
+    def Tamaño_actual(self):
+        return
+
+    def Obtener_conocimiento(self, linea, columna):
+        return
